1ddb73bec1c0e66bfb6f7b7cee6de72c0faee9b4
ahmadalasaad <ahmadalserese9754@gnail.com>
2021-09-10 02:42:47 +0300

    a

--- server.js
+++ server.js
@@ -2,17 +2,19 @@
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 const cors = require('cors');
-const getWeather= require('./controller/Forecast.controller')
-const getMovies=require('./controller/Movies.controller')
+const getWeather = require('./controller/Forecast.controller')
+const getMovies = require('./controller/Movies.controller')
 app.use(cors())
 require('dotenv').config();
-// a server endpoint 
+// a server endpoint
+const PORT = process.env.PORT;
 app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-});
+  function (req, res) { // callback function of what we should do with our request
+    res.send('Hello World') // our endpoint function response
+  });
 
 
-app.get('/weather',getWeather);
-app.get('/movies',getMovies);
-app.listen(3002) // kick start the express server to work
+app.get('/weather', getWeather);
+app.get('/movies', getMovies);
+
+app.listen(PORT) // kick start the express server to work
