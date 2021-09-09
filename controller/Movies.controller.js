b1944616fb44113fe7a11a07f9d166dd3ceda5a1
ahmadalasaad <ahmadalserese9754@gnail.com>
2021-09-09 15:14:01 +0300

    class10

--- controller/Movies.controller.js
+++ controller/Movies.controller.js
@@ -2,15 +2,50 @@ const MOVIE_API_KEY =process.env.MOVIE_API_KEY;
 const axios = require('axios');
 const Movies = require('../models/Movies.model');
 require('dotenv').config();
+const Cache = require('../helper/cache.helper');
+let cacheObject = new Cache();
+
+console.log('================');
+console.log('Cache instance created');
+console.log('================');
 
 const getMovies= async (request, response) => {
-    const city_name=request.query.query;
+    const query=request.query.query;
+    const InMilSec = 50000;
+    const oneDayPassed = (Date.now() - cacheObject.timeStamp) > InMilSec;
+    if (oneDayPassed) {
+      console.log('================');
+      console.log('Cache Reset');
+      console.log('================');
+      cacheObject = new Cache();
+    }
+
+    const foundData = cacheObject.foreCast.find(location => location.lat === lat && location.lon === lon);
+   
+    if (foundData) {
+      response.json(foundData.data);
+    } else {
+      console.log('No Cache data found');
+      console.log('================');
+
     const moviesUrl='https://api.themoviedb.org/3/search/movie'
-    const moviesResponse= await axios.get(`${moviesUrl}?api_key=${MOVIE_API_KEY}&query=${city_name}`)
-    if (city_name) {
-    let newArr=moviesResponse.data.results.map(element=>{
+    const moviesResponse= await axios.get(`${moviesUrl}?api_key=${MOVIE_API_KEY}&query=${query}`)
+    if (query) {
+    let data=moviesResponse.data.results.map(element=>{
       return new Movies(element.title,element.overview,element.vote_average,element.vote_count,element.poster_path,element.popularity,element.release_date);  })
-    response.send(newArr)
+      console.log('================');
+      console.log('Save data into cache');
+      console.log('================');
+
+      cacheObject.movies.push({
+        "query": query,
+        "data": data
+      });
+      console.log('================');
+      console.log(cacheObject);
+      console.log('================');
+    response.send(data)
     }else{response.send('no data found :disappointed:')}
+  }
   }
   module.exports = getMovies;
---
+++ helper/cache.helper.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class Cache {
+  constructor() {
+    this.foreCast = [];
+    this.movies = [];
+    this.timeStamp = Date.now();
+  }
+}
+
+module.exports = Cache;
